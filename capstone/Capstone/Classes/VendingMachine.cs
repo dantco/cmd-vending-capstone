737003d56bbe04e69ba72bcb36904fbbe143af92
Nikki Burkhart <nlburkhart@gmail.com>
2022-06-10 10:34:09 -0400

    updating capstone

--- capstone/Capstone/Classes/VendingMachine.cs
+++ capstone/Capstone/Classes/VendingMachine.cs
@@ -16,13 +16,72 @@ namespace Capstone.Classes
         //CONSTRUCTOR
         public VendingMachine()
         {
-         
+
         }
 
         //METHODS
-        
-        
+        public void DisplayMenu1()
+        {
+            Console.WriteLine("Welcome to the Umbrella Corp Vendo-Matic 800(TM)");
+            Console.WriteLine("Please make a selection...");
+            Console.WriteLine("(1) Display Vending Machine Items");
+            Console.WriteLine("(2) Purchase");
+            Console.WriteLine("(3) Exit");
+            //make sure to account for the error if the user doesn't enter a valid entry at this point
+        }
 
+        public void SoundEffect(string itemType)
+        {
+            if (itemType == "Chip")
+            {
+                Console.WriteLine("Crunch Crunch, Yum!");
+            }
+            else if (itemType == "Candy")
+            {
+                Console.WriteLine("Munch Munch, Yum!");
+            }
+            else if (itemType == "Drink")
+            {
+                Console.WriteLine("Slurp Slurp, Yum!");
+            }
+            else if (itemType == "Gum")
+            {
+                Console.WriteLine("Chewy Chewy, Yum!");
+            }
+        }
+
+        public virtual Dictionary<string, int> ReturnChange(decimal totalMoneyProvided)
+        {
+            Dictionary<string, int> changeDictionary = new Dictionary<string, int>();
+                       
+            int quarters = 0;
+            int dimes = 0;
+            int nickels = 0;
+
+            while (totalMoneyProvided > 0.00M)
+            {
+
+                if (totalMoneyProvided >= 0.25M)
+                {
+                    quarters += 1;
+                    totalMoneyProvided -= 0.25M;
+                }
+                else if(totalMoneyProvided >= 0.10M)
+                {
+                    dimes += 1;
+                    totalMoneyProvided -= 0.10M;
+                }
+                else if(totalMoneyProvided >= 0.05M)
+                {
+                    nickels += 1;
+                    totalMoneyProvided -= 0.05M;
+                }
+            }
+            changeDictionary.Add("Quarters", quarters);
+            changeDictionary.Add("Dimes", dimes);
+            changeDictionary.Add("Nickels", nickels);
+            return changeDictionary;
+        }
     }
 }
 
--- capstone/Capstone/Program.cs
+++ capstone/Capstone/Program.cs
@@ -10,10 +10,11 @@ namespace Capstone.Classes
         public static void Main(string[] args)
         {
 
-            //Dictionary<string, VendingInventory> inventoryDictionary = new Dictionary<string, VendingInventory>();
-
+            //Instantiate the vending machine
             VendingMachine vendingMachine = new VendingMachine();
+            Dictionary<string, int> changeDictionary = new Dictionary<string, int>();
 
+            //populate the inventory list and/or restock
             try
             {
                 using (StreamReader sr = new StreamReader(@"C:\Users\Student\git\c-sharp-mini-capstone-module-1-team-3\capstone\vendingmachine.csv"))
@@ -21,7 +22,7 @@ namespace Capstone.Classes
 
                     while (!sr.EndOfStream)
                     {
-                        string slotLocation = "";               // the following code takes information from document and adds to dictionary
+                        string slotLocation = "";// the following code takes information from document and adds to dictionary
                         VendingInventory itemGroup = new VendingInventory();
 
                         string line = sr.ReadLine();
@@ -40,69 +41,108 @@ namespace Capstone.Classes
                 Console.WriteLine(exception.Message);
             }
 
-            Console.WriteLine("Welcome to the Umbrella Corp Vendo-Matic 800(TM)");
-            Console.WriteLine("Please make a selection...");
-            Console.WriteLine("(1) Display Vending Machine Items");
-            Console.WriteLine("(2) Purchase");
-            Console.WriteLine("(3) Exit");
+            //DISPLAY MENU 1 FIRST TIME
+            vendingMachine.DisplayMenu1();
 
-            int userInput = int.Parse(Console.ReadLine());
+            //CAPTURE MENU 1 SELECTION
+            int menu1ActionSelect = int.Parse(Console.ReadLine());
 
-            if (userInput == 1)
+            //LOOP THROUGH MENU 1
+            while (menu1ActionSelect != 3)
             {
-                foreach (KeyValuePair<string, VendingInventory> name in vendingMachine.inventoryDictionary)
+                if (menu1ActionSelect == 1) //IF USER SELECTED DISPLAY ITEM LIST
                 {
-                    Console.WriteLine($"{name.Key}, {name.Value.ItemName}, ${name.Value.ItemPrice} ({name.Value.ItemQuantity} REMAINING)");
-                    //make sure there is a method that reduces itemQuantity
-                    //if item quantity == 0, SOLD OUT
+                    foreach (KeyValuePair<string, VendingInventory> name in vendingMachine.inventoryDictionary)
+                    {
+                        Console.WriteLine($"{name.Key}, {name.Value.ItemName}, ${name.Value.ItemPrice} ({name.Value.ItemQuantity} REMAINING)");
+                        //make sure there is a method that reduces itemQuantity
+                        //if item quantity == 0, SOLD OUT
+                    }
                 }
-            }
-            else if (userInput == 4)
-            {
-
-            }
-            else if (userInput == 3)
-            {
-                Console.WriteLine("Thank you for shopping!");
-            }
-            else if (userInput == 2)
-            {
-                decimal totalMoneyProvided = 0.00M;
-                
-                Console.WriteLine($"Current money provided: {totalMoneyProvided}");
-                Console.WriteLine();
-                Console.WriteLine("Please select an option:");
-                Console.WriteLine("(1) Feed Money");
-                Console.WriteLine("(2) Select Product");
-                Console.WriteLine("(3) Finish Transaction");
-
-                int actionSelect = int.Parse(Console.ReadLine());
-                while (actionSelect != 3)
+                else if (menu1ActionSelect == 4)  //IF USER SELECTS HIDDEN OPTION
                 {
-                    if (actionSelect == 1)
-                    {
-                        Console.WriteLine("Enter the amount to deposit in whole dollars.");
-                        decimal inputMoney = decimal.Parse(Console.ReadLine());
-                        totalMoneyProvided += inputMoney;
-                        Console.WriteLine($"You have put a total of {totalMoneyProvided} in the machine.");
-                    }
-                    else if (actionSelect == 2)
-                    {
 
-                    }
+                }
+                else if (menu1ActionSelect == 3) //IF USER SELECTS EXIT
+                {
+                    Console.WriteLine("Thank you for shopping!");
+                }
+                else if (menu1ActionSelect == 2) //IF USER SELECTS PURCHASE ITEMS
+                {
+                    decimal totalMoneyProvided = 0.00M;
 
-
-                    Console.WriteLine($"Current money provided: {totalMoneyProvided}");
+                    Console.WriteLine();
+                    Console.WriteLine($"Current money provided: ${totalMoneyProvided}");
                     Console.WriteLine();
                     Console.WriteLine("Please select an option:");
                     Console.WriteLine("(1) Feed Money");
                     Console.WriteLine("(2) Select Product");
                     Console.WriteLine("(3) Finish Transaction");
 
-                    actionSelect = int.Parse(Console.ReadLine()); 
+                    int actionSelect = int.Parse(Console.ReadLine());
+                    while (actionSelect != 3)
+                    {
+                        if (actionSelect == 1)
+                        {
+                            Console.WriteLine("Enter the amount to deposit in whole dollars.");
+                            decimal inputMoney = decimal.Parse(Console.ReadLine());
+                            totalMoneyProvided += inputMoney;
+                            Console.WriteLine($"You have put a total of ${totalMoneyProvided} in the machine.");
+                            Console.WriteLine();
+                        }
+                        else if (actionSelect == 2)
+                        {
+                            foreach (KeyValuePair<string, VendingInventory> name in vendingMachine.inventoryDictionary)
+                            {
+                                Console.WriteLine($"{name.Key}, {name.Value.ItemName}, ${name.Value.ItemPrice} ({name.Value.ItemQuantity} REMAINING)");
+           
+                                //make sure there is a method that reduces itemQuantity
+                                //if item quantity == 0, SOLD OUT
+                            }
+                            Console.WriteLine("Enter code of product to purchase:");
+                            string itemSelection = Console.ReadLine();
+                            if(!vendingMachine.inventoryDictionary.ContainsKey(itemSelection.ToUpper()))
+                            {
+                                Console.WriteLine("Does Not Exist!");
+                            }
+                            else if(vendingMachine.inventoryDictionary[itemSelection.ToUpper()].ItemQuantity<=0)
+                            {
+                                Console.WriteLine("Sold Out");
+                            }
+                            else
+                            {
+                                vendingMachine.SoundEffect(vendingMachine.inventoryDictionary[itemSelection.ToUpper()].ItemType);
+                                totalMoneyProvided -= vendingMachine.inventoryDictionary[itemSelection.ToUpper()].ItemPrice;
+                                vendingMachine.inventoryDictionary[itemSelection.ToUpper()].ItemQuantity -= 1;
+                            }
+                        }
 
-                }
-            };
+                        Console.WriteLine($"Current money provided: ${totalMoneyProvided}");
+                        Console.WriteLine();
+                        Console.WriteLine("Please select an option:");
+                        Console.WriteLine("(1) Feed Money");
+                        Console.WriteLine("(2) Select Product");
+                        Console.WriteLine("(3) Finish Transaction");
+
+                        actionSelect = int.Parse(Console.ReadLine());
+
+                    }
+
+                    if (actionSelect == 3)
+                    {
+                        changeDictionary = vendingMachine.ReturnChange(totalMoneyProvided);
+                        Console.WriteLine("Your change:");
+                        Console.WriteLine($"Quarters : {changeDictionary["Quarters"]}");
+                        Console.WriteLine($"Dimes : {changeDictionary["Dimes"]}");
+                        Console.WriteLine($"Nickels : {changeDictionary["Nickels"]}");
+                    }
+
+                };
+
+                //DISPLAY MENU 1 SECOND TIME BEFORE LOOPING BACK
+                vendingMachine.DisplayMenu1();
+                menu1ActionSelect = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
